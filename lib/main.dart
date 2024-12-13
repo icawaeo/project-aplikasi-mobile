a464a9e53e2ae69603bf86add009365553b220e1
Miracle Sumajow <miraclesumajow32@gmail.com>
2024-12-13 21:32:18 +0800

    Adding 3 new pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,241 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Currency Converter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.blue,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MainPageState createState() => MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  static final List<Widget> _pages = <Widget>[
+    const HomePage(),
+    const CurrencyConverterPage(),
+    const LatestRatesPage(),
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money),
+            label: 'Convert',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.schedule),
+            label: 'Historical Rates',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Welcome to Currency Converter App',
+        style: TextStyle(fontSize: 24),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CurrencyConverterPage extends StatefulWidget {
+  const CurrencyConverterPage({super.key});
+
+  @override
+  CurrencyConverterPageState createState() => CurrencyConverterPageState();
+}
+
+class CurrencyConverterPageState extends State<CurrencyConverterPage> {
+  String _fromCurrency = "USD";
+  String _toCurrency = "IDR";
+  double _amount = 0.0;
+  double _result = 0.0;
+
+  // Simbol mata uang
+  final Map<String, String> _currencySymbols = {
+    "USD": "\$",
+    "IDR": "Rp",
+    "EUR": "€",
+    "JPY": "¥",
+    "AUD": "A\$",
+  };
+
+  Future<void> _convertCurrency() async {
+    final url = Uri.parse(
+        "https://api.currencyfreaks.com/latest?apikey=e4133672ad34470dbce972116594876b");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final rates = data['rates'];
+      if (rates.containsKey(_fromCurrency) && rates.containsKey(_toCurrency)) {
+        setState(() {
+          _result = _amount *
+              (double.parse(rates[_toCurrency]) /
+                  double.parse(rates[_fromCurrency]));
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Currency not found')),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Failed to fetch conversion rate')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(labelText: 'Amount'),
+            onChanged: (value) {
+              _amount = double.tryParse(value) ?? 0.0;
+            },
+          ),
+          DropdownButton<String>(
+            value: _fromCurrency,
+            items: ['USD', 'IDR', 'EUR', 'JPY', 'AUD']
+                .map((currency) => DropdownMenuItem(
+                      value: currency,
+                      child: Text(currency),
+                    ))
+                .toList(),
+            onChanged: (value) {
+              setState(() {
+                _fromCurrency = value!;
+              });
+            },
+          ),
+          DropdownButton<String>(
+            value: _toCurrency,
+            items: ['USD', 'IDR', 'EUR', 'JPY', 'AUD']
+                .map((currency) => DropdownMenuItem(
+                      value: currency,
+                      child: Text(currency),
+                    ))
+                .toList(),
+            onChanged: (value) {
+              setState(() {
+                _toCurrency = value!;
+              });
+            },
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _convertCurrency,
+            child: const Text('Convert'),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Result: ${_currencySymbols[_toCurrency] ?? ""} $_result',
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LatestRatesPage extends StatelessWidget {
+  final String _apiKey = "e4133672ad34470dbce972116594876b";
+
+  const LatestRatesPage({super.key});
+
+  Future<Map<String, dynamic>> fetchLatestRates() async {
+    final url = Uri.parse(
+        "https://api.currencyfreaks.com/v2.0/rates/latest?apikey=$_apiKey");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to fetch latest rates');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Latest Rates')),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: fetchLatestRates(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final data = snapshot.data;
+            final rates = data?['rates'] as Map<String, dynamic>;
+
+            return ListView(
+              padding: const EdgeInsets.all(16.0),
+              children: rates.entries.map((entry) {
+                return ListTile(
+                  title: Text('${entry.key}'),
+                  subtitle: Text('${entry.value}'),
+                );
+              }).toList(),
+            );
+          }
+        },
+      ),
     );
   }
 }
